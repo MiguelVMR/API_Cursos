2f1a62f77c5a9853a1fdf56237fe3733d4d82827
Miguel <miguelmvilela12@gmail.com>
2023-11-23 16:29:58 -0300

    Terminado CRUD base

--- src/main/java/br/com/fepi/cursos/controller/CursoController.java
+++ src/main/java/br/com/fepi/cursos/controller/CursoController.java
@@ -5,9 +5,11 @@ package br.com.fepi.cursos.controller;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,5 +47,29 @@ public class CursoController {
     public Curso create(@RequestBody Curso curso){
 
         return repository.save(curso);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Curso> update(@PathVariable Long id, @RequestBody Curso curso){
+        
+        return repository.findById(id)
+        .map(r -> {
+            r.setNome(curso.getNome());
+            r.setCategoria(curso.getCategoria());
+            Curso update = repository.save(r);
+            return ResponseEntity.ok().body(r);
+        })
+        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?>  delete(@PathVariable Long id){ 
+        return repository.findById(id)
+        .map(r -> {
+            repository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        })
+        .orElse(ResponseEntity.notFound().build());
+        
     }
 }
